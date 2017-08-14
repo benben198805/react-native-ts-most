2f099c37f3a2fae6753bf388d49a7e8b60a51032
qlyan(benben) <qlyan@thoughtworks.com>
2017-08-15 01:00:05 +0800

    [#] add product list

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import Price from './Price/Price';
+import User from './User/User';
+import ProductDetail from './ProductDetail/ProdcutDetail';
+
+export {
+  Price,
+  User,
+  ProductDetail,
+};
---
+++ src/modules/home/reducer.ts
@@ -0,0 +1,22 @@
+import * as D from '../../definitions';
+import * as Redux from 'redux';
+
+const initialState: D.HomeProductsState = {
+    products: [],
+};
+
+const homeReducer: Redux.Reducer<D.HomeProductsState> = (state: D.HomeProductsState, action: D.GeneralAction): D.HomeProductsState => {
+    state = state || initialState;
+    switch (action.type) {
+        case 'FETCH_HOME_PRODUCTS_SUC':
+            const products = action.payload as D.Product[];
+            return {
+                ...state,
+                products,
+            };
+        default:
+    }
+    return state;
+};
+
+export default homeReducer;
