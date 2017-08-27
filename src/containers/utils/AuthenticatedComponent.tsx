21a34a2d2ac8fdaa95653bb565ff4fcfb14e138c
qlyan(benben) <qlyan@thoughtworks.com>
2017-08-27 21:54:34 +0800

    [#] fix popup login page twice

--- src/containers/utils/AuthenticatedComponent.tsx
+++ src/containers/utils/AuthenticatedComponent.tsx
@@ -1,32 +1,36 @@
 import React, { ComponentType } from 'react';
 import { connect, DispatchProp } from 'react-redux';
-import { NavigationActions } from 'react-navigation'
-import { View } from "react-native";
+import { NavigationActions, NavigationScreenProp, NavigationAction } from 'react-navigation';
+import { View, NavigationState } from "react-native";
 
 import * as D from '../../definitions'
-import { isEqual, includes } from 'lodash'
+import { isEqual, includes, get } from 'lodash';
 
 export type AuthProps<S> = DispatchProp<S> & {
   user: D.User,
   app: D.App,
   nav: any,
   navigate: typeof NavigationActions.navigate,
+  navigation: NavigationScreenProp<NavigationState, NavigationAction>,
 }
 
 export function requireAuthentication(Component: ComponentType): ComponentType {
 
   class AuthenticatedComponent extends React.Component<AuthProps<object>, object>{
-    // componentWillMount() {
-    //   this.goToLoginPage();
-    // }
+    
     componentWillReceiveProps(nextProps) {
       const nextRouterIndex = this.getRouterTabIndex(nextProps.nav)
       const authRouterIndexs = [1, 2]
+      const currentRouteName = get(this.props.navigation.state, 'routeName')
       if (!this.isStatePropsSame(nextProps)
+        && currentRouteName === nextProps.nav.routes[nextRouterIndex].routes[0].routeName
         && !nextProps.user.username
         && nextProps.nav.routes[nextRouterIndex].routes.length <= 2
-        && this.props.nav.index == 0
+        && this.props.nav.index !== nextProps.nav.index
         && includes(authRouterIndexs, nextRouterIndex)) {
+          console.log(this.props)
+          console.log(nextProps)
+          
         this.goToLoginPage();
       };
     }
